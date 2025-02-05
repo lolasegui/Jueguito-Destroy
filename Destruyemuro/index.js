39c2c843ea3940efbc5b5de055b03a34ff79116e
lolasegui <lsegui19939@iesjoanramis.org>
2025-02-05 12:01:24 +0100

    Add files via upload

--- Destruyemuro/index.js
+++ Destruyemuro/index.js
@@ -3,6 +3,41 @@ const ctx = canvas.getContext("2d")
 
 canvas.height = 512;
 canvas.width = 448;
+let colors = ["#c953e8","#7b1c7e","#8f62d9", "#ed3f76","#da1772","#3b376f"]
+
+
+//variables des Mexicans
+const filas = 6
+const columnes = 12
+const ampleMur= 30;
+const alturaMur = 14;
+const margeTMur = 80;
+const margeEMur = 30;
+const sepMurs = 2;
+
+const murs= []
+const ESTAT_MUR = {
+    DESTRUIT: 0,
+    SHOW: 1
+}
+
+for( let c= 0; c<columnes; c++){
+    murs[c] = [];
+    for(let f=0; f<filas; f++){
+        const color =colors[Math.floor(Math.random()*6)]
+        const murX = margeEMur+c* (ampleMur+sepMurs)
+        const murY = margeTMur+f*(alturaMur+sepMurs)
+        murs[c][f] = {
+            x: murX,
+            y: murY,
+            status: ESTAT_MUR.SHOW,
+            color: color
+
+        }
+
+
+    }
+}
 
 //variables pilota
 let radiPilota = 10;
@@ -32,6 +67,7 @@ let vida=3;
 
 
 
+
 function pintarPilota() {
     ctx.beginPath();
     ctx.arc(x, y, radiPilota, 0, Math.PI * 2);
@@ -48,7 +84,18 @@ function pintarPala() {
 }
 
 function pintarMurs() {
+    for( let c= 0; c<columnes; c++){
+        for(let f=0; f<filas; f++){
+            const murActual = murs[c][f];
+            if(murActual.status == ESTAT_MUR.DESTRUIT)
+                continue;
+        
+        ctx.fillStyle = murActual.color;
+        ctx.rect(murActual.x,murActual.y,ampleMur,alturaMur)
+        ctx.fill();
+    }
 
+}
 }
 function deteccioColisio() {
 
@@ -66,35 +113,37 @@ function movimentPilota() {
         dy= -dy;
     }
 
-    // game over
-    if(y + dy > canvas.height){
-        vida -=1;
-        if(vida>0){
-            x =canvas.width/2
-            y = canvas.height -40;
-            palaX =(canvas.width -amplePala)/2
-            palaY =canvas.height - alturaPala -10;
-        }else{
-            document.location.reload();
-            ctx.font = '24px Serif'
-            ctx.textAlign = 'center'
-            ctx.fillText("Game Over", canvas.width/2, canvas.height/2)
-            setTimeout(()=>{
 
-            },50000)
-        }
-        console.log("GAME OVER")
-        document.location.reload();
+    const mateixaX = x > palaX && x < palaX + amplePala;
+    const mateixaY = y + dy > palaY
+
+    // game over
+    if(mateixaX && mateixaY){
+        dy = -dy;
+
+    }else if (y + dy > canvas.height){
+            vides --
+            x = canvas.width / 2
+            y = canvas.height -30
+            dx = 2
+            dy = -2
+
+            if(vida == 0){
+                console.log ("GAME OVER")
+                document.location.reload();
+            }
+
     }
+
 
     
     // moviment
     x += dx
     y += dy
-
+}
 
     
-}
+
 
 function movimentPala() {
     if(dreta && palaX <canvas.width -amplePala){
